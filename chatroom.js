05867770d29f986f0090477922e96d6b80f757bb
Xingyu Chen <chenxingyu.cs@gmail.com>
2015-08-20 23:16:25 +0800

    add some simple changes
    DEBUG=myapp npm start

--- chatroom.js
+++ chatroom.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! Test');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(4000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
