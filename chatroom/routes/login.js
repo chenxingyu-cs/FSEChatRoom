d273f69073b2346b7f7366795f7e49d84a71ba9f
Xingyu Chen <chenxingyu.cs@gmail.com>
2015-08-21 10:47:09 +0800

    add login logic

--- chatroom/routes/login.js
+++ chatroom/routes/login.js
@@ -9,7 +9,38 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res) {
   console.log("login received");
-  console.log(req.body);
+  var fs = require("fs");
+  var file = "chat.db";
+  var exists = fs.existsSync(file);
+  if(!exists) {
+    console.log("Creating DB file.");
+    fs.openSync(file, "w");
+  }
+
+  //Create the chat-history Table
+  var sqlite3 = require("sqlite3").verbose();
+  var db = new sqlite3.Database(file);
+
+  db.serialize(function() {
+    if(!exists) {
+      db.run("CREATE TABLE ChatHistory ( userName TEXT,  msgContent TEXT,  msgTime DATETIME DEFAULT CURRENT_TIMESTAMP)");
+      db.run("CREATE TABLE UserInfo ( userId INTEGER PRIMARY KEY, userName TEXT, account TEXT, password TEXT, lastLoginTime DATETIME DEFAULT CURRENT_TIMESTAMP)");
+    }  
+  });
+  db.each("SELECT count(*) as num, userId, userName, password FROM UserInfo where account ='" +req.body.account + "'" , function(err, row) {
+    if(row.num != 0){
+      if(row.password == req.body.password){
+        console.log(row.userId + ': ' + row.userName + " which psw is: " + row.password);
+      }else{
+        console.log("psw wrong");
+      }
+    }else{
+      console.log("no account exits");
+    }
+  })
+
+  db.close();
+
   
 });
 
