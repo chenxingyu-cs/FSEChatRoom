6ea70db0941e0131c4a1d785ea9923fafac79ebc
Xingyu Chen <chenxingyu.cs@gmail.com>
2015-08-21 09:32:58 +0800

    complete the register

--- chatroom/routes/register.js
+++ chatroom/routes/register.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+//create the database file
+var fs = require("fs");
+var file = "chat.db";
+var exists = fs.existsSync(file);
+if(!exists) {
+  console.log("Creating DB file.");
+  fs.openSync(file, "w");
+}
+
+//Create the chat-history Table
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(file);
+
+db.serialize(function() {
+  if(!exists) {
+    db.run("CREATE TABLE ChatHistory ( userName TEXT,  msgContent TEXT,  msgTime DATETIME DEFAULT CURRENT_TIMESTAMP)");
+    db.run("CREATE TABLE UserInfo ( userId INTEGER PRIMARY KEY, userName TEXT, account TEXT, password TEXT, lastLoginTime DATETIME DEFAULT CURRENT_TIMESTAMP)");
+  }  
+});
+
+var stmtRegister = db.prepare("INSERT INTO UserInfo (userName, account, password) VALUES (?, ?, ?)");
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('register', { title: 'register' });
@@ -9,8 +31,14 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res) {
   console.log("register received");
-  console.log(req.body);
-  
+  stmtRegister.run(req.body.username, req.body.account, req.body.password);
+  stmtRegister.finalize();
+db.each("SELECT rowid AS id, userName, account, password FROM UserInfo", function(err, row) {
+  console.log(row.id + ": " + row.userName+ '   ' + row.account + '   ' + row.password);
 });
+db.close();
+});
+
+
 
 module.exports = router;
